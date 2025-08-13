c122d5dda286034434635a9af465cf9f4f3303db
Abedallah-Sako <127647063+Abedallah-Sako@users.noreply.github.com>
2025-08-13 13:22:35 +0300

    add an option to bypass certificate validation

--- LabibaVoiceAssistant/src/main/java/ai/labiba/labibavoiceassistant/network/RetrofitClient.kt
+++ LabibaVoiceAssistant/src/main/java/ai/labiba/labibavoiceassistant/network/RetrofitClient.kt
@@ -1,12 +1,24 @@
 package ai.labiba.labibavoiceassistant.network
 
 import ai.labiba.labibavoiceassistant.sdkSetupClasses.LabibaVA
+import ai.labiba.labibavoiceassistant.sdkSetupClasses.LabibaVAInternal
+import android.os.Build
 import android.util.Log
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.net.InetSocketAddress
+import java.net.Proxy
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSession
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
 object RetrofitClient {
 
@@ -18,8 +30,14 @@ object RetrofitClient {
         return if (mClient != null) {
             mClient!!
         } else {
-            mClient = getClient(getLogging())
-            mClient ?: getClient(getLogging())
+
+            if(LabibaVAInternal.labibaVaTheme.themeSettings.useUnsafeOkHttpClient){
+                mClient = getUnsafeOkHttpClient()
+                mClient ?: getUnsafeOkHttpClient()
+            }else{
+                mClient = getClient(getLogging())
+                mClient ?: getClient(getLogging())
+            }
         }
     }
 
@@ -61,5 +79,75 @@ object RetrofitClient {
             .writeTimeout(2, TimeUnit.MINUTES)
             .readTimeout(2, TimeUnit.MINUTES)
             .build()
+
+
+    fun getUnsafeOkHttpClient(): OkHttpClient {
+        try {
+            // Create a trust manager that does not validate certificate chains
+
+            val trustManager: X509TrustManager = object : X509TrustManager {
+                @Throws(CertificateException::class)
+                override fun checkClientTrusted(
+                    chain: Array<X509Certificate?>?,
+                    authType: String?
+                ) {
+                }
+
+                @Throws(CertificateException::class)
+                override fun checkServerTrusted(
+                    chain: Array<X509Certificate?>?,
+                    authType: String?
+                ) {
+                }
+
+                override fun getAcceptedIssuers(): Array<X509Certificate?> {
+                    return arrayOf<X509Certificate?>()
+                }
+            }
+            val trustAllCerts = arrayOf<TrustManager>(trustManager)
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+
+
+            //            final SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, SecureRandom())
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.getSocketFactory()
+
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(
+                sslSocketFactory,
+                (trustAllCerts[0] as javax.net.ssl.X509TrustManager?)!!
+            )
+            builder.hostnameVerifier(object : HostnameVerifier {
+                override fun verify(hostname: String?, session: SSLSession?): Boolean {
+                    return true
+                }
+            })
+
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+
+            builder.addInterceptor(logging)
+            builder.readTimeout(
+                2,
+                TimeUnit.MINUTES
+            )
+            builder.writeTimeout(
+                2,
+                TimeUnit.MINUTES
+            )
+            builder.connectTimeout(
+                2,
+                TimeUnit.MINUTES
+            )
+
+            return builder.build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
 
 }
--- LabibaVoiceAssistant/src/main/java/ai/labiba/labibavoiceassistant/sdkSetupClasses/builders/labiba_theme/LabibaVAThemeSettings.kt
+++ LabibaVoiceAssistant/src/main/java/ai/labiba/labibavoiceassistant/sdkSetupClasses/builders/labiba_theme/LabibaVAThemeSettings.kt
@@ -6,24 +6,27 @@ class LabibaVAThemeSettings {
     internal var isAutoListening = false
     internal var isTextToSpeechEnabled = true
     internal var autoDetectLanguage = true
+    internal var useUnsafeOkHttpClient = false
 
 
     class Builder {
         private var _isAutoListening = LabibaVAThemeSettings().isAutoListening
         private var _isTextToSpeechEnabled = LabibaVAThemeSettings().isTextToSpeechEnabled
         private var _autoDetectLanguage = LabibaVAThemeSettings().isTextToSpeechEnabled
+        private var _useUnsafeOkHttpClient = LabibaVAThemeSettings().useUnsafeOkHttpClient
 
 
         fun enableAutoListening(enable: Boolean) = apply { this._isAutoListening = enable }
         fun enableTextToSpeech(enable: Boolean) = apply { this._isTextToSpeechEnabled = enable }
         fun enableAutoDetectLanguage(enable: Boolean) = apply { this._autoDetectLanguage = enable }
+        fun useUnsafeOkHttpClient(use: Boolean) = apply { this._useUnsafeOkHttpClient = use }
 
 
         fun build() = LabibaVAThemeSettings().apply {
             isTextToSpeechEnabled = _isTextToSpeechEnabled
             isAutoListening = _isAutoListening
             autoDetectLanguage = _autoDetectLanguage
-
+            useUnsafeOkHttpClient = _useUnsafeOkHttpClient
         }
     }
 
